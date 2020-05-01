71d5651646b869b035ac7f155ab0900ff87abb01
clecio <clecio.antao@gmail.com>
2020-05-01 07:08:24 -0300

    Correção suprimindo Success corrigido

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -131,12 +131,12 @@ public class TasksTest {
 			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
 			driver.findElement(By.id("saveButton")).click();
 			String message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Sucess!", message);
+			Assert.assertEquals("Success!", message);
 			
 			//remover a Tarefa
 			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
 			message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Sucess!", message);
+			Assert.assertEquals("Success!", message);
 		} finally {			
 			//fechar o browser
 			driver.quit();
