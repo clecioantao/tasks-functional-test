080d4b5f7205ad9cae699b3ff4c4536e73ee0a15
clecioantao <clecio.antao@gmail.com>
2022-07-19 09:16:31 -0300

    Ajustes Health Check

--- src/test/java/br/ce/wcaquino/tasks/prod/HealthCheckIT.java
+++ src/test/java/br/ce/wcaquino/tasks/prod/HealthCheckIT.java
@@ -16,9 +16,9 @@ public class HealthCheckIT {
 	@Test
 	public void healthCheck() throws MalformedURLException {
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.32:4444/wd/hub"), cap);
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.2.72:4444/wd/hub"), cap);
 		try {
-			driver.navigate().to("http://192.168.0.36:8003/tasks/");
+			driver.navigate().to("http://192.168.2.61:8003/tasks/");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			String version = driver.findElement(By.id("version")).getText();
 			Assert.assertTrue(version.startsWith("build"));
